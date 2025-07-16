7edea15593a06455de35d6d1c6c95b910ec26725
João Gabriel <jgluciano.luz@gmail.com>
2025-07-16 14:09:23 -0300

    dump data superuser

--- config/settings.py
+++ config/settings.py
@@ -85,9 +85,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+if os.environ.get("DATABASE_URL"):
+    # Ambiente de produção (ex: Render)
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600)
+    }
+else:
+    # Ambiente local (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
