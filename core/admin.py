7795bf19c1d2094f35429c8c278cf63954242960
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 19:16:26 -0300

    feat(cliente): registra Clientes ao painel admin com filtragem nativa

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cliente
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'documento', 'telefone', 'email']
+    list_filter = ['cidade', 'uf']
+    search_fields = ['nome', 'documento', 'email']
+    list_per_page = 20
