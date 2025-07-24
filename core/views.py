48812bebf922d71c86b9c141c4c090413762dbd1
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 17:52:01 -0300

    feat(cliente): implementa métodos PUT/DEL á função ClienteDetailAPIView para manipular modelo CLiente

--- core/views.py
+++ core/views.py
@@ -18,3 +18,35 @@ class ClienteListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClienteDetailAPIView(APIView):
+    def get_object(self, id):
+        try:
+            return Cliente.objects.get(id=id)
+        except Cliente.DoesNotExist:
+            return None
+
+    def get(self, request, id):
+        cliente = self.get_object(id)
+        if not cliente:
+            return Response({'erro': 'Cliente não encontrado'}, status=404)
+        serializer = ClienteSerializer(cliente)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        cliente = self.get_object(id)
+        if not cliente:
+            return Response({'erro': 'Cliente não encontrado'}, status=404)
+        serializer = ClienteSerializer(cliente, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request, id):
+        cliente = self.get_object(id)
+        if not cliente:
+            return Response({'erro': 'Cliente não encontrado'}, status=404)
+        cliente.delete()
+        return Response(status=204)
