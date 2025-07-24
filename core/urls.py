e532e3bfcea61e69b575f18126b23ffb2749ad3d
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 14:00:13 -0300

    chore: inclui rota /api e rota para API view

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import ClienteListCreateAPIView
 
 urlpatterns = [
-  path('clientes/', ClienteListCreateAPIView.as_view(), name='cliente-list-create'),
+    path('clientes/', ClienteListCreateAPIView.as_view(),
+         name='cliente-list-create'),
 ]
