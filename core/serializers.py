5beb7a500415250458767298baa4b1a50934820c
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 19:28:15 -0300

    refactor(cliente): adiciona o getstatusdisplay para exibir o valor amigavel na requisição

--- core/serializers.py
+++ core/serializers.py
@@ -3,6 +3,11 @@ from .models import Cliente
 
 
 class ClienteSerializer(serializers.ModelSerializer):
+    status_display = serializers.SerializerMethodField()
+
     class Meta:
         model = Cliente
         fields = '__all__'
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
