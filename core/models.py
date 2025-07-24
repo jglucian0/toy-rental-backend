46023e21f22f00a99b54cead57fd21d0332d6092
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 12:47:03 -0300

    feat(cliente): cria modelo Cliente e realiza as migrações

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    STATUS_CHOICES = [
+        ('ativo', 'Ativo'),
+        ('inativo', 'Inativo'),
+    ]
+
+    nome = models.CharField(max_length=100)  # Orbigatório
+    documento = models.CharField(max_length=14)  # Orbigatório
+    email = models.EmailField(null=True, blank=True)
+    telefone = models.CharField(max_length=11)  # Orbigatório
+    status = models.CharField(
+        max_length=7, choices=STATUS_CHOICES, default='ativo')  # Orbigatório
+    cep = models.CharField(max_length=8, null=True, blank=True)
+    endereco = models.CharField(max_length=100)  # Orbigatório
+    numero = models.CharField(max_length=5, null=True, blank=True)
+    cidade = models.CharField(max_length=100, null=True, blank=True)
+    uf = models.CharField(max_length=2, null=True, blank=True)
+    complemento = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.documento}'
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-patterns = [
-    path('/', views.proximas_festas, name='proximas_festas'),
-]
