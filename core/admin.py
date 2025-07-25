6f4b6274093c388005bd94df3b3bc5b8c32998c2
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 23:54:06 -0300

    feature(brinquedo): registra o modelo Brinquedo no painel admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Brinquedo
 
 
 @admin.register(Cliente)
@@ -7,4 +7,10 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ['nome', 'documento', 'telefone', 'email']
     list_filter = ['cidade', 'uf']
     search_fields = ['nome', 'documento', 'email']
+    list_per_page = 20
+    
+@admin.register(Brinquedo)
+class BrinquedoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    search_fields = ['nome']
     list_per_page = 20
