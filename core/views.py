eab4546d9f991d298b5f2f5cbadc131a55765d92
João Gabriel <jgluciano.luz@gmail.com>
2025-07-25 00:01:47 -0300

    feat(brinquedo): adicona ao BrinquedoAPIView método POST

--- core/views.py
+++ core/views.py
@@ -64,3 +64,10 @@ class BrinquedoListCreateAPIView(APIView):
         brinquedos = Brinquedo.objects.all()
         serializer = BrinquedoSerializer(brinquedos, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = BrinquedoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
