d21f04e8271b54977994d347cfa4b689f662a751
João Gabriel <jgluciano.luz@gmail.com>
2025-07-24 22:55:38 -0300

    feat(brinquedo): cria o modelo Brinquedos com os campos necessários

--- core/models.py
+++ core/models.py
@@ -22,3 +22,31 @@ class Cliente(models.Model):
 
     def __str__(self):
         return f'{self.nome} - {self.documento}'
+
+
+class Brinquedo(models.Model):
+    STATUS_CHOICES = [
+        ('ativo', 'Ativo'),
+        ('inativo', 'Inativo'),
+    ]
+
+    VOLTAGEM_CHOICES = [
+        ('110v', '110v'),
+        ('220v', '220v'),
+        ('bivolt', 'Bivolt'),
+    ]
+
+    nome = models.CharField(max_length=100)
+    valor_diaria = models.DecimalField(max_digits=8, decimal_places=2)
+    qtd_total = models.IntegerField()
+    qtd_disponivel = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, default='ativo')
+    tamanho = models.CharField(max_length=10)
+    voltagem = models.CharField(
+        choices=VOLTAGEM_CHOICES, null=True, blank=True)
+    energia = models.BooleanField(null=True, blank=True)
+    inflavel = models.BooleanField(null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.nome} - {self.valor_diaria}'
