297c66f7658766deb1eddcbad6534ad062c68ef7
João Gabriel <jgluciano.luz@gmail.com>
2025-09-02 18:07:33 -0300

    Restore Organization owner link

--- core/models.py
+++ core/models.py
@@ -9,8 +9,8 @@ import uuid
 
 class Organization(models.Model):
     name = models.CharField(max_length=200)
-    #owner = models.ForeignKey(
-    #    User, on_delete=models.CASCADE, related_query_name='owned_organizations')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_query_name='owned_organizations')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
