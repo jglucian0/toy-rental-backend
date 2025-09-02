2b954f7cdaa6f748611c99735a990f47782d49ba
João Gabriel <jgluciano.luz@gmail.com>
2025-09-02 01:24:20 -0300

    deploy: cria superuser via management

--- core/management/commands/create_custom_superuser.py
+++ core/management/commands/create_custom_superuser.py
@@ -10,13 +10,13 @@ class Command(BaseCommand):
     help = 'Cria um superusuário de forma não interativa usando variáveis de ambiente'
 
     def handle(self, *args, **options):
-        username = os.environ.get('jgluciano ')
-        email = os.environ.get('jgluciano.luz@gmail.com')
-        password = os.environ.get('@Agbdlcid10')
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
         if not all([username, email, password]):
             self.stdout.write(self.style.ERROR(
-                'As variáveis de ambiente jgluciano, jgluciano.luz@gmail.com e @Agbdlcid10 devem ser definidas.'))
+                'As variáveis de ambiente DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL e DJANGO_SUPERUSER_PASSWORD devem ser definidas.'))
             return
 
         if User.objects.filter(username=username).exists():
